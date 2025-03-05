5c484f8ab04bdb29c61b573e3ad5c35f9ca252e6
LUISJLD <ldperezf@unimagdalena.edu.co>
2025-03-04 20:28:32 -0500

    Actualizacion Query and Query Methods

---
+++ src/main/java/edu/unimagdelan/aeropuerto/Entiti/AerolineaRepository.java
@@ -0,0 +1,34 @@
+package edu.unimagdelan.aeropuerto.Entiti;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface AerolineaRepository extends JpaRepository<Aerolinea, Long> {
+
+
+        List<Aerolinea> findByNombreContaining(String nombre);
+        List<Aerolinea> findByVuelosDestino(String destino);
+        List<Aerolinea> findByVuelosOrigen(String origen);
+        List<Aerolinea> findByVuelosNumeroVuelo(String numeroVuelo);
+        boolean existsByNombre(String nombre);
+
+
+        @Query("SELECT a FROM Aerolinea a WHERE a.nombre LIKE %:nombre%")
+        List<Aerolinea> obtenerAerolineasPorNombre(@Param("nombre") String nombre);
+
+        @Query("SELECT DISTINCT a FROM Aerolinea a JOIN a.vuelos v WHERE v.destino = :destino")
+        List<Aerolinea> obtenerAerolineasConVuelosADestino(@Param("destino") String destino);
+
+        @Query("SELECT DISTINCT a FROM Aerolinea a JOIN a.vuelos v WHERE v.origen = :origen")
+        List<Aerolinea> obtenerAerolineasConVuelosDesdeOrigen(@Param("origen") String origen);
+
+        @Query("SELECT DISTINCT a FROM Aerolinea a JOIN a.vuelos v WHERE v.numeroVuelo = :numeroVuelo")
+        List<Aerolinea> obtenerAerolineasPorNumeroVuelo(@Param("numeroVuelo") String numeroVuelo);
+
+        @Query("SELECT CASE WHEN COUNT(a) > 0 THEN TRUE ELSE FALSE END FROM Aerolinea a WHERE a.nombre = :nombre")
+        boolean existeAerolineaPorNombre(@Param("nombre") String nombre);
+
+
+}
